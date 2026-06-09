a56ea30f7cf5a80835d485cffc07f3cca2f0a7d7
RoisT-lab <contacto@roistrade.com>
2026-06-08 20:24:31 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const config = window.ROIS_CONFIG || {};
-const roisBuild = "20260608-unified-light-platform-v9";
+const roisBuild = "20260608-black-header-logo-v10";
 const demoMode = config.demoMode !== false || !config.supabaseUrl || !config.supabaseAnonKey;
 const storeKey = "rois_demo_data_v2";
 const sessionKey = "rois_session_v2";
