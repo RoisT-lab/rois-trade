f09ffc4031ff4123198c19885260c6ee3d54bc6c
RoisT-lab <contacto@roistrade.com>
2026-06-11 19:47:49 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const config = window.ROIS_CONFIG || {};
-const roisBuild = "20260611-logo-tone-v25";
+const roisBuild = "20260611-logo-transparent-v26";
 const roisLegalEntity = "IntelliQuant S.A.P.I. de C.V.";
 const athleteAnnualExemptEmails = ["saidr1521@gmail.com"];
 const demoMode = config.demoMode !== false || !config.supabaseUrl || !config.supabaseAnonKey;
