0f2eee075f24d3f8b6002b2289e07ce711f1441a
RoisT-lab <contacto@roistrade.com>
2026-07-06 15:14:15 -0600

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -5,7 +5,7 @@
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqY2ZlZHlvc3N0c25vZHJ6Y3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2Mjc1MTgsImV4cCI6MjA5NjIwMzUxOH0.Zvkn3Z3zZA6bopGVF_p3RCtp1y-xADj7JDhOgmYQhsM",
   notificationEmailWebhook: "",
   stripePaymentLinks: {
-    eventRegistration: "https://buy.stripe.com/eVqbJ0exAgeU0dUdeO18c00",
+    eventRegistration: "https://buy.stripe.com/7sY8wO0GKd2I1hY7Uu18c0b",
     athleteAnnualProfile: "https://buy.stripe.com/aFadR8blo6Ek5yedeO18c07",
     roisPartnerMonthly: "https://buy.stripe.com/bJe00igFI3s8d0GfmW18c04",
     officialSponsorMonthly: "https://buy.stripe.com/7sY9AS3SW7Iod0GcaK18c02",
