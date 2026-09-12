97a8051312337e1431e5e3c4c79ad407d3c7fc6a
RoisT-lab <contacto@roistrade.com>
2026-09-11 22:38:32 -0600

    feat: apply proposal-inspired design to home and dashboards

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const config = window.ROIS_CONFIG || {};
-const roisBuild = "20260911-official-brand";
+const roisBuild = "20260912-proposal-design";
 const sponsorshipLevelsStorageKey = "rois_sponsorship_levels_v1";
 const roisSponsorshipFeeRate = 0.3;
 const ROIS_CREATIVE_FEE_RATE = 0.30;
